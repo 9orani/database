e3b7642cbbad61bdaa369417868be7c1c7938a49
shasuri <ghimmk@gmail.com>
2022-08-10 01:44:12 +0900

    temp commit

--- script/sql_copy.sh
+++ script/sql_copy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+
 docker cp gorani_db_manager:/var/lib/pgadmin/storage/gorani_gorani.com/sql_files/$(date +%Y%m%d)_gorani_init.sql sql/
+
+sed 'i/DROP DATABASE/\connect postgres/' 20220808_gorani_init.sql
+
 cp sql/$(date +%Y%m%d)_gorani_init.sql sql/recent_gorani_init.sql
 
